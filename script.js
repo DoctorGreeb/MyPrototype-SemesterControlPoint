d0fbd67dfc5a3631bc183df53c9ed4215bd16cfd
DoctorGreeb <dr.dre.odyssey@gmail.com>
2024-12-24 13:14:18 +0300

    new update

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+function performSearch() {
+    const query = document.querySelector('#input1').value;
+    if (!query) {
+        alert('Пожалуйста, введите запрос!');
+        return;
+    }
+    const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`; // Замените на ваш URL
+    console.log('Поиск:', searchUrl);
+    window.location.href = searchUrl;
+}
